e6f8ef066e0d8cb1410383c8493da79040c81450
zhes3129 <62133520+zhes3129@users.noreply.github.com>
2020-05-15 16:28:35 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -6,6 +6,7 @@ from linebot.models import TemplateSendMessage,ConfirmTemplate, MessageTemplateA
 from linebot.models import ImagemapSendMessage, BaseSize, MessageImagemapAction, URIImagemapAction, ImagemapArea, TemplateSendMessage, ButtonsTemplate, DatetimePickerTemplateAction
 from linebot.models import TextSendMessage, AudioSendMessage, VideoSendMessage
 from linebot.models import TextSendMessage, BubbleContainer, ImageComponent, BoxComponent, TextComponent, IconComponent, ButtonComponent, SeparatorComponent, FlexSendMessage, URIAction
+from hotelapi.models import booking, users
 
 import datetime
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
@@ -522,3 +523,176 @@ def manageForm(event, mtext):
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
 
+def sendUse(event):  #使用說明
+    try:
+        text1 ='''
+1. 「房間預約」及「取消訂房」可預訂及取消訂房。每個 LINE 帳號只能進行一個預約記錄。
+2. 「關於我們」對旅館做簡單介紹及旅館圖片。
+3. 「位置資料」列出旅館地址，並會顯示地圖。
+4. 「聯絡我們」可直接撥打電話與我們聯繫。
+               '''
+        message = TextSendMessage(
+            text = text1
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendBooking(event, user_id):  #房間預約
+    try:
+        if not (booking.objects.filter(bid=user_id).exists()):  #沒有訂房記錄
+            message = TemplateSendMessage(
+                alt_text = "房間預約",
+                template = ButtonsTemplate(
+                    thumbnail_image_url='https://i.imgur.com/1NSDAvo.jpg',
+                    title='房間預約',
+                    text='您目前沒有訂房記錄，可以開始預訂房間。',
+                    actions=[
+                        URITemplateAction(label='房間預約', uri='line://app/1580167687-86z4YMdx')  #開啟LIFF讓使用者輸入訂房資料
+                    ]
+                )
+            )
+        else:  #已有訂房記錄
+            message = TextSendMessage(
+                text = '您目前已有訂房記錄，不能再訂房。'
+            )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendCancel(event, user_id):  #取消訂房
+    try:
+        if booking.objects.filter(bid=user_id).exists():  #已有訂房記錄
+            bookingdata = booking.objects.get(bid=user_id)  #讀取訂房資料
+            roomtype = bookingdata.roomtype
+            amount = bookingdata.roomamount
+            in_date = bookingdata.datein
+            out_date = bookingdata.dateout
+            text1 = "您預訂的房間資料如下："
+            text1 += "\n房間型式：" + roomtype
+            text1 += "\n房間數量：" + amount
+            text1 += "\n入住日期：" + in_date
+            text1 += "\n退房日期：" + out_date
+            message = [
+                TextSendMessage(  #顯示訂房資料
+                    text = text1
+                ),
+                TemplateSendMessage(  #顯示確認視窗
+                    alt_text='取消訂房確認',
+                    template=ConfirmTemplate(
+                        text='你確定要取消訂房嗎？',
+                        actions=[
+                            PostbackTemplateAction(  #按鈕選項
+                                label='是',
+                                data='action=yes'
+                            ),
+                            PostbackTemplateAction(
+                                label='否',
+                                data='action=no'
+                           )
+                        ]
+                    )
+                )
+            ]
+        else:  #沒有訂房記錄
+            message = TextSendMessage(
+                text = '您目前沒有訂房記錄！'
+            )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendAbout(event):  #關於我們
+    try:
+        text1 = "我們提供良好的環境及優質的住宿服務，使您有賓至如歸的感受，歡迎來體驗美好的經歷。"
+        message = [
+            TextSendMessage(  #旅館簡介
+                text = text1
+            ),
+            ImageSendMessage(  #旅館圖片
+                original_content_url = "https://i.imgur.com/1NSDAvo.jpg",
+                preview_image_url = "https://i.imgur.com/1NSDAvo.jpg"
+            ),
+        ]
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendPosition(event):  #位置資訊
+    try:
+        text1 = "地址：南投縣埔里鎮信義路85號"
+        message = [
+            TextSendMessage(  #顯示地址
+                text = text1
+            ),
+            LocationSendMessage(  #顯示地圖
+                title = "宜居旅舍",
+                address = text1,
+                latitude = 23.97381,
+                longitude = 120.977198
+            ),
+        ]
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendContact(event):  #聯絡我們
+    try:
+        message = TemplateSendMessage(
+            alt_text = "聯絡我們",
+            template = ButtonsTemplate(
+                thumbnail_image_url='https://i.imgur.com/tVjKzPH.jpg',
+                title='聯絡我們',
+                text='打電話給我們',
+                actions=[
+                    URITemplateAction(label='撥打電話', uri='tel:0123456789')  #開啟打電話功能
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def manageForm(event, mtext, user_id):  #處理LIFF傳回的FORM資料
+    try:
+        flist = mtext[3:].split('/')  #去除前三個「#」字元再分解字串
+        roomtype = flist[0]  #取得輸入資料
+        amount = flist[1]
+        in_date = flist[2]
+        out_date = flist[3]
+        unit = booking.objects.create(bid=user_id, roomtype=roomtype, roomamount=amount, datein=in_date, dateout=out_date)  #寫入資料庫
+        unit.save()
+        text1 = "您的房間已預訂成功，資料如下："
+        text1 += "\n房間型式：" + roomtype
+        text1 += "\n房間數量：" + amount
+        text1 += "\n入住日期：" + in_date
+        text1 += "\n退房日期：" + out_date
+        message = TextSendMessage(  #顯示訂房資料
+            text = text1
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendYes(event, user_id):  #處理取消訂房
+    try:
+        datadel = booking.objects.get(bid=user_id)  #從資料庫移除資料記錄
+        datadel.delete()
+        message = TextSendMessage(
+            text = "您的房間預訂已成功刪除。\n期待您再次預訂房間，謝謝！"
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def pushMessage(event, mtext):  ##推播訊息給所有顧客
+    try:
+        msg = mtext[6:]  #取得訊息
+        userall = users.objects.all()
+        for user in userall:  #逐一推播
+            message = TextSendMessage(
+                text = msg
+            )
+            line_bot_api.push_message(to=user.uid, messages=[message])  #推播訊息
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
